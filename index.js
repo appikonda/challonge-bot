28ac9accd3d05d6d7dc6677d8cec0aae91db235e
Sandeep Appikonda <sandeep@appikonda.in>
2019-11-26 22:26:37 -0800

    implement bot

--- index.js
+++ index.js
@@ -4,109 +4,148 @@ const challonge = require('challonge');
 const Telegraf = require('telegraf');
 
 const client = challonge.createClient({
-    apiKey: process.env.challonge_api_key
+  apiKey: process.env.challonge_api_key
 });
 
-let statsMap = new Map();
-function getPlayer(callback) {
+const bot = new Telegraf(process.env.telegram_bot_key);
+bot.start((ctx) => ctx.reply('Welcome!'));
+bot.hears('/table', async (ctx) => {
+  // implement me
+  // step 1 - get players
+  const players = await getPlayers();
+  //console.log(players)
+  const playerDetails = buildPlayerDetails(players);
+ // console.log(playerDetails)
+  // step 2 - get matches
+  const matches = await getMatches();
+  // step 3 - mashup info
+  const table = getTable(players, matches, playerDetails);
+  const formattedTable = formatTable(table);
+  ctx.reply(formattedTable);
+});
+bot.launch();
+
+async function getPlayers() {
+  return new Promise((resolve, reject) => {
     client.participants.index({
-        id: process.env.tournament_id,
-        callback
+      id: process.env.tournament_id,
+      callback: (err, players) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(players);
+        }
+      }
     });
+  });
 }
-getPlayer((err, data) => {
-    if (data) {
-        let playerKeys = Object.keys(data);
-        playerKeys.map(key => {
-            let playerInfomation = data[key].participant
-            // create player details
-            let playerDetails = {
-                name: playerInfomation.name,
-                goalsFor: 0,
-                goalsAgainst: 0,
-                won: 0,
-                lost: 0,
-                draw: 0
-            };
-            statsMap.set(playerInfomation.id, playerDetails);
-        });
-        // Get match information
-        client.matches.index({
-            id: process.env.tournament_id,
-            callback: (err, matchData) => {                
-                if (matchData) {
-                    let matches = Object.keys(matchData);
-                    matches.map(key => {
-                        let matchInfo = matchData[key].match;
 
-                        if (matchInfo.state == 'complete') {
-                            let player1Id = matchInfo.player1Id;
-                            let player2Id = matchInfo.player2Id;
-                            let score = matchInfo.scoresCsv.split("-");
-                            let player1score = score[0];
-                            let player2score = score[1];
-                          // console.log(typeof (matchInfo.winnerId) + '' + matchInfo.winnerId)
-                           // console.log(typeof (player1Id) + '' + player1Id)
+async function getMatches() {
+  return new Promise((resolve, reject) => { 
+    client.matches.index({
+      id: process.env.tournament_id,
+      callback: function(error, matches){
+        if(error){
+          reject(error)
+        }else{
+          resolve(matches)
+        }
 
-                            switch (matchInfo.winnerId) {
-                                case player1Id:
-                                    //update player1  details - winner
-                                    updatePlayerDetails(player1Id, player1score, player2score, 1, 0, 0);
-                                    //update player2  details - loser
-                                    updatePlayerDetails(player2Id, player2score, player1score, 0, 1, 0);
-
-                                    break
-                                case player2Id:
-                                    //update player1  details - winner
-                                    updatePlayerDetails(player2Id, player2score, player1score, 1, 0, 0);
-
-                                    //update player2  details - loser
-                                    updatePlayerDetails(player1Id, player1score, player2score, 0, 1, 0);
-
-                                    break;
-
-                                default:
-                                    //update player1  details - draw
-                                    updatePlayerDetails(player1Id, player1score, player2score, 0, 0, 1);
-                                    //update player2  details - draw
-                                    updatePlayerDetails(player2Id, player2score, player1score, 0, 0, 1);
+      }
+    });
 
 
+  });
+  
+}
 
-                            } // end of switch
+function buildPlayerDetails(players){
+  const playerDetails = {};
+  //console.log(Object.keys(players));
+  for(let index  of Object.keys(players)){
+    const player = players[index].participant
+    playerDetails[player.id] = { name: player.name, w: 0, l: 0, d: 0, ga: 0, gf: 0 };
+    //console.log(playerDetails)
+  }
+  
+
+  return playerDetails;
+}
 
 
-                        }
+function getTable(players, matches, playerDetails){
 
-                    })
-                    var table = '<b>Standings</b>\n------------------------------\n   Name  | GF | GA | W | L | D |    \n--------------------------------\n';
-
-                    for (let pid of statsMap.keys()) {
-                        let playerinfo = statsMap.get(pid);
-                        table += (playerinfo.name.slice(0, 8) + ' | ' + (playerinfo.goalsFor) + ' | ' + parseInt(playerinfo.goalsAgainst) + ' | ' + playerinfo.won + ' | ' + playerinfo.lost + ' | ' + playerinfo.draw)
-                        table += ('\n')
-                    }
-                    //telegram call
-                    const bot = new Telegraf(process.env.telegram_bot_key);
-                    bot.start((ctx) => ctx.replyWithHTML(HTML))
-                    bot.hears('/table', (ctx) => ctx.replyWithHTML(table))
-                    bot.launch()
-
-                }
-
-            }
-        }); // Matches Clinet
-
+  for (let index of Object.keys(matches)){
+    const match = matches[index].match;
+    if(match.state == 'complete'){
+      const outcome = determineMatchoutcome(match)
+      let [firstPlayerGoals, secondPlayerGoals] = match.scoresCsv.split('-');
+      firstPlayerGoals = parseInt(firstPlayerGoals,10);
+      secondPlayerGoals = parseInt(secondPlayerGoals,10);
+      if(outcome.result === 'noTie'){
+        const winningGoals =  Math.max(firstPlayerGoals, secondPlayerGoals);
+        const losingGoals =  Math.min(firstPlayerGoals, secondPlayerGoals);
+        updateWinner(playerDetails, outcome.winnerId, winningGoals, losingGoals)
+        updateLoser(playerDetails, outcome.loserId, losingGoals, winningGoals)
+      }else{
+        updateTiedPlayers(playerDetails, outcome.firstPlayer, outcome.secondPlayer, firstPlayerGoals)
+      }
     }
-}); // get player
+  }
+  return playerDetails;
+}
+
+function formatTable(table){
+  let formattedTable = '';
+  for( let playerId of Object.keys(table)){
+    const playerDetails = table[playerId];
+    formattedTable +=  [playerDetails.name, playerDetails.w, playerDetails.d, playerDetails.l, playerDetails.gf, playerDetails.ga].join('|');
+    formattedTable += "\n";
+
+  }
+
+  return formattedTable;
+}
+
+function determineMatchoutcome(match){
+  
+
+  const outcome = { winnerId : null, loserId : null, result : '', firstPlayer : null, secondPlayer : null}
+  if(match.winnerId != null){
+    outcome.winnerId = match.winnerId;
+    outcome.loserId = match.loserId;
+    outcome.result = 'noTie';
+  }else{
+    console.log(match)
+    outcome.firstPlayer = match.player1Id;
+    outcome.secondPlayer = match.player2Id;
+    outcome.result = 'tie';
+  }
+
+  return outcome;
+}
+
+function updatePlayerDetails(playerDetails, playerId, goalsFor, goalsAgainst, win, draw, loss){
+  const currPlayer = playerDetails[playerId];
+  //console.log(Object.keys(playerDetails), playerId);
+  currPlayer.gf += goalsFor;
+  currPlayer.ga += goalsAgainst;
+  currPlayer.w += win;
+  currPlayer.d += draw;
+  currPlayer.l += loss;
+}
 
 
-function updatePlayerDetails(pid, gs, ga, w, l, d) {
-    let player = statsMap.get(pid);
-    statsMap.get(pid).goalsFor = Number(player.goalsFor) + Number(gs);
-    statsMap.get(pid).goalsAgainst = Number(player.goalsAgainst) + Number(ga);
-    statsMap.get(pid).won = player.won + w;
-    statsMap.get(pid).lost = player.lost + l;
-    statsMap.get(pid).draw = player.draw + d;
-  //  console.log(statsMap.get(pid))
+function updateWinner(playerDetails, playerId, goalsFor, goalsAgainst){
+  updatePlayerDetails(playerDetails, playerId, goalsFor, goalsAgainst, 1, 0, 0);
+
+}
+
+function updateLoser(playerDetails, playerId, goalsFor, goalsAgainst){
+  updatePlayerDetails(playerDetails, playerId, goalsFor, goalsAgainst, 0, 0, 1);
+}
+
+function updateTiedPlayers(playerDetails, firstPlayerId, secondPlayerId, goals){
+  updatePlayerDetails(playerDetails, firstPlayerId, goals, goals, 0, 1, 0);
+  updatePlayerDetails(playerDetails, secondPlayerId, goals, goals, 0, 1, 0);
 }
